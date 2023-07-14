27df97f0216380da1c6a62177e8142e87d60384c
ansen01-svg <ansenbeyc73@gmail.com>
2023-07-14 21:35:09 +0530

    customised mui theme

--- src/app_providers/mui_provider/theme.js
+++ src/app_providers/mui_provider/theme.js
@@ -1,7 +1,38 @@
 import { createTheme } from "@mui/material";
 
 
-const theme = createTheme({});
+const theme = createTheme({
+    typography: {
+      fontFamily: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+      ].join(','),
+      h1: {
+        fontSize: '25px',
+        fontWeight: '500'
+      }
+    },
+    components: {
+      MuiCssBaseline: {
+        styleOverrides: `
+          @font-face {
+            font-family: '-apple-system';
+            font-style: normal;
+            font-display: swap;
+            font-weight: 400;          
+        }
+        `,
+      },
+    },
+  });
 
 
 export default theme;
